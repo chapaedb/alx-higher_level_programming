a82344c2011d673d8125fe4cb3b7656d5b0cf01a
chapaedb <chapaeresso1@gmail.com>
2023-10-04 16:11:50 -0700

    Task 6..

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 for j in range(0, 10):
     for k in range(j + 1, 10):
         if j == 8 and k == 9:
             print("{}{}".format(j, k))
         else:
             print("{}{}".format(j, k), end=",")
-
