8e484acde00d087a01584c11dca69b7c2a118ae8
chapaedb <chapaeresso1@gmail.com>
2023-10-06 14:00:02 -0700

    Task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,20 @@
+import dis
+
+if __name__ == "__main__":
+    with open("hidden_4.pyc", "rb") as file:
+        bytecode = file.read()
+
+    names = []
+    instructions = dis.get_instructions(bytecode)
+    for instruction in instructions:
+        if (
+            instruction.opname == "LOAD_CONST"
+            and isinstance(instruction.argval, str)
+            and not instruction.argval.startswith("__")
+        ):
+            names.append(instruction.argval)
+
+    names.sort()
+
+    for name in names:
+        print(name)
