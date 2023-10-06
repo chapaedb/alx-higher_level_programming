15018abf4047905f53cfe79c17560665513bab67
chapaedb <chapaeresso1@gmail.com>
2023-10-06 11:02:16 -0700

    Donee

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+
 from calculator_1  import add, sub, div, mul
 a = 10
 b = 5
