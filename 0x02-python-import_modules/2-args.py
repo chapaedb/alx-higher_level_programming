a1eda24312200fec1374497a2cee248849a5e542
chapaedb <chapaeresso1@gmail.com>
2023-10-06 13:26:06 -0700

    Fg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,14 @@ if __name__ == "__main__":
 
     num = len(sys.argv) - 1
     if num == 0:
+
         print("{} arguments.".format(num))
     elif num == 1:
-        print("{}: argument".format(num))
-        print("{}: {}".format(num, sys.argv[num]))
+
+        print("{}: argument:".format(num))
+        print("{}:{}".format(num, sys.argv[num]))
     else:
+
         print("{} arguments:".format(num))
         for i in range(1, num + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{}:{}".format(i, sys.argv[i]))
