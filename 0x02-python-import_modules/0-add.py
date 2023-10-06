177cd37915eb618eca3847cdf1f5633ef36ee057
chapaedb <chapaeresso1@gmail.com>
2023-10-06 10:05:14 -0700

    Doc

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-from add_0 import add
-
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
-=======
-from add_0 import add
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
 a = 1
 b = 2
