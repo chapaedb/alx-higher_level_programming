5132ed6ecdfbd5583d77606fb836443e15e6439f
chapaedb <chapaeresso1@gmail.com>
2023-10-06 13:03:34 -0700

    Finally

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-    """Print the number of and list of arguments."""
+if name == "main":
+    """Handle basic arithmetic operations."""
     import sys
-
     num = len(sys.argv) - 1
-    print("Number of argument(s):", num, end="")
-
     if num == 0:
-        print(".", end="\n\n")
+        print("{} arguments.".format(num))
+    elif num == 1:
+        print("{} argument:".format(num))
+        print("{}: {}".format(num, sys.argv[num]))
     else:
-        print(":", end="\n")
-        for key in range(1, num + 1):
-            print("{}: {}".format(key, sys.argv[key]))            
+        print("{} arguments:".format(num))
+        for i in range(1, num + 1):
+            print("{}: {}".format(i, sys.argv[i]))
