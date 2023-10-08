fe3f9892046fd70bb78f86da2b69f989cce448b9
chapaedb <chapaeresso1@gmail.com>
2023-10-08 12:06:03 -0700

    Task2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,12 +7,3 @@ def replace_in_list(my_list, idx, element):
         new_list = my_list[:]
         new_list[idx] = element
         return new_list
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = replace_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
