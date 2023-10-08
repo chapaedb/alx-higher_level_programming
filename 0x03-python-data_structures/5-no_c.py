663e2e57590e976bf6896ad49f28d2c906f3cbc6
chapaedb <chapaeresso1@gmail.com>
2023-10-08 13:28:30 -0700

    Done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,8 @@
 def no_c(my_string):
     mystr = ""
     for x in my_string:
-        if x != 'c' and x != 'C':
+        if x == 'c' or x == 'C':
+            continue
+        else:
             mystr += x
     return mystr
