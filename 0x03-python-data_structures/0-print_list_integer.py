ff066ad39aea5c2f55d7f6419abef16d91d5188d
chapaedb <chapaeresso1@gmail.com>
2023-10-08 11:45:05 -0700

    Task 0.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,10 +2,4 @@
 
 def print_list_integer(my_list=[]):
     for x in my_list:
-        print("{}".format(x))
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-
-    print_list_integer(my_list)
+        print("{:d}".format(my_list[x]))
