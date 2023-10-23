d12d1ef5f3cd1243a4aa1d853bbae9c4e9787b11
chapaedb <chapaeresso1@gmail.com>
2023-10-23 11:50:08 +0300

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for count in range(x):
+            print("{}".format(my_list[x]),end="")
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
