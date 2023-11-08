c2c7e33f9e12aefc8eb45dab0ce98cc5d16add03
chapaedb <chapaeresso1@gmail.com>
2023-11-08 14:25:53 -0800

    Done

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,41 +1,23 @@
+7-main.py
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+bg = BaseGeometry()
 
-class BaseGeometry:
-    """
-    Represents the base geometry.
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
 
-    This class serves as a base for other geometry classes.
-    """
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-    def area(self):
-        """
-        Calculate the area of the geometry.
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-        This method is not implemented in the base class.
-
-        Raises:
-            Exception: If the area() method is called on the base class.
-
-        Returns:
-            None.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, param_name, param_value):
-        """
-        Validate a parameter as an integer.
-
-        Args:
-            param_name (str): The name of the parameter.
-            param_value (int): The parameter to validate.
-
-        Raises:
-            TypeError: If param_value is not an integer.
-            ValueError: If param_value is less than or equal to 0.
-        """
-        if type(param_value) != int:
-            raise TypeError("{} must be an integer".format(param_name))
-        if param_value <= 0:
-            raise ValueError("{} must be greater than 0".format(param_name))
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
