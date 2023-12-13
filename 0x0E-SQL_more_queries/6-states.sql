63ce7c5aa34c47f2de13c314008df42d1a07407a
chapaedb <chapaeresso1@gmail.com>
2023-12-13 12:47:56 -0800

    Added doc

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 --- create a ccdb and a user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use the db
 USE hbtn_0d_usa;
 -- creates a table
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
