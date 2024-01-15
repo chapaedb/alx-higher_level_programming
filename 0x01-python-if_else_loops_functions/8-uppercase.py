93a70c3adf009f0a92711b5087e57533d57ae3f0
chapaedb <chapaeresso1@gmail.com>
2024-01-15 11:40:17 -0800

    DOne

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def uppercase(string):
     for char in string:
         val = ord(char)
         if val >= 97 and val <= 122:
-            ascii_val -= 32
+            ascii_val = val - 32
         print("{}".fotmat(chr(ascii_val)), end = "")
     
