2c85de8b669994fd4a08843dadd6ab9759a1291c
chapaedb <chapaeresso1@gmail.com>
2024-01-16 23:24:56 -0800

    Update

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+  my_list = str(num)
+    length= len(my_list)
+    print("{}".format(my_list[length - 1]), end="")
+    return my_list[length - 1]
