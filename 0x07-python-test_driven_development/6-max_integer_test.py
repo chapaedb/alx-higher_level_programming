ae841115c24dffe5b7be71547f8cfbfd14bbd24b
chapaedb <chapaeresso1@gmail.com>
2024-02-04 12:18:42 -0800

    Done

---
+++ 0x07-python-test_driven_development/6-max_integer_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+   return i max(for i in list)
+
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+from my_module import max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_positive_numbers(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
+        self.assertEqual(max_integer([1, -3, 4, -2]), 4)
+
+if __name__ == '__main__':
+    unittest.main()
