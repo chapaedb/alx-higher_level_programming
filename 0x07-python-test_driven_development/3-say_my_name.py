6097b23bfd687e1fffa80255a12259416acda9d4
chapaedb <chapaeresso1@gmail.com>
2024-02-04 05:12:19 -0800

    Function added

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def say_my_name(first_name, last_name=""):
+    """Prints "My name is <first name> <Last name>"""
+    if not isinstance(first_name, string):
+    raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
