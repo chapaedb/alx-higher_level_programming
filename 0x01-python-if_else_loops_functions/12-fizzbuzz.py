8c7c6f6b3d02416b465a67237fd5828f8fa2f23a
chapaedb <chapaeresso1@gmail.com>
2024-02-04 22:00:31 -0800

    DOne

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@
 def fizzbuzz():
     """ Prints fizz for multiple of 3
     buzz for 5 and fizzbuzz for multiple of both"""
-    for i in range(100):
+    for i in range(1,101):
         if i % 3 == 0:
             print("Fizz")
         elif i % 5 == 0:
