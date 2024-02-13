06e98a59891e4b6890e7920bc0390fa6638723da
chapaedb <chapaeresso1@gmail.com>
2024-02-13 04:43:55 -0800

    Done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 let count = process.argv.length - 2;
-if( count === 0){
-	console.log(“No argument”);
-}
-else if (count === 1){
-	console.log(“Argument found”);}
 
-else{
-	console.log(“Arguments found”);}
+if (count === 0) {
+  console.log("No argument");
+} else if (count === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
