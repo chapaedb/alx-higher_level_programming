4b07c445c09d4c86cc4b471ed10bca0c5c9237af
chapaedb <chapaeresso1@gmail.com>
2024-02-17 00:27:35 -0800

    Update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], conn=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id""")
+    cur.execute(""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id OREDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
