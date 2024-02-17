f1ad7a179584d2a4fbc7e18e6d252755160d4be5
chapaedb <chapaeresso1@gmail.com>
2024-02-17 10:27:20 -0800

    Update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.name FROM cities INNER JOIN states ON states.id = cities.state_id WHERE states.name = %s", (sys.argv[4],))
     rows = cur.fetchall()
     result =  [ row[0] for row in rows]
-        print(", ".join(result))
+        print(result, sep = ",")
     cur.close()
     db.close()
