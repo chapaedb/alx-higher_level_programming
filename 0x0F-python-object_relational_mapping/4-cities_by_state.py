cab0a44921ae8a843d77127df772f1b252e7e1ef
chapaedb <chapaeresso1@gmail.com>
2024-02-18 13:57:34 -0800

    Update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+"""  lists all states from the database hbtn_0e_0_us"""
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], conn=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id ORDER BY id ASC""")
-    rows = cur.fetchall()
-    for row in rows:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+
+    cursor = db.cursor()
+
+    query = """
+        SELECT cities.id, cities.name, states.name 
+        FROM cities 
+        JOIN states ON cities.state_id = states.id 
+        ORDER BY cities.id ASC
+    """
+
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    for row in results:
         print(row)
-    cur.close()
-    conn.close()
+
+    cursor.close()
+    db.close()
