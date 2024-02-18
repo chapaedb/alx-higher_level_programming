3da46b161964f9109e90d36e9de0d9163ee0b170
chapaedb <chapaeresso1@gmail.com>
2024-02-18 13:32:58 -0800

    UPdate

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,13 +4,11 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],passwd=sys.argv[2], conn=sys.argv[3], port=3306)
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], conn=sys.argv[3], port=3306)
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
-         print(row)
+        print(row)
     cur.close()
     conn.close()
-
-
